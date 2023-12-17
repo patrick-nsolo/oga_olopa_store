adb0ca4f81be54e44e7b204fbc3523abf44a81ac
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-12-17 17:43:54 +0100

    update react website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+//Edit website interface [App.js] here 
+
 import './App.css';
+import About from './components/AboutMe/About';
+import Contact from './components/ContactForm/Contact';
+import Gallery from './components/Gallery/Gallery';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Projects from './components/MyProjects/Projects';
+import Tools from './components/Tools/Tools';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Home/>
+      <Projects/>
+      <Gallery/>
+      <About/>
+      <Tools/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className='about' id='about'>
+        <h2>Who is Oga Olopa?</h2> 
+        <p>
+        Oga Olopa stands as a premier online destination dedicated to meeting the 
+        diverse security needs of individuals and businesses alike. Specializing 
+        in the retail of cutting-edge security equipment, Oga Olopa provides a 
+        comprehensive range of products designed to ensure safety and peace of mind.
+        </p>
+        <p>
+        Our extensive catalog encompasses a wide array of state-of-the-art security
+        solutions, including surveillance cameras, access control systems, alarm systems, 
+        and personal safety devices. Oga Olopa carefully curates its selection, 
+        partnering with renowned brands and manufacturers known for their commitment 
+        to quality and innovation.
+        </p>
+    </div>
+  )
+}
+
+export default About;

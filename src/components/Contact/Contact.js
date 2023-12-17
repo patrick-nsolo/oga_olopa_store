d8b04167075d3aa3ae971bbfa92e42dee2dcd6f2
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-12-17 19:06:31 +0100

    update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,9 +8,9 @@ const Contact = () => {
         <h2>Lets Connect!</h2>
         <p>Follow us on all social media! Email us here</p>  
         <div class="contact-links">
-            <a className='m-3' href='https://github.com/patrick-nsolo' target='_blank' rel='noreferrer'><i class="fa-brands fa-github icon"></i></a>
+            <a className='m-3' href='https://github.com/patrick-nsolo' target='_blank' rel='noreferrer'><i class="fa-brands fa-instagram icon"></i></a>
             <a className='m-3' href='https://www.facebook.com/hotboi2thamost' target='_blank' rel='noreferrer'><i class="fa-brands fa-facebook icon"></i></a>
-            <a className='m-3' href='https://www.linkedin.com/in/patrick-nsolo-83b095a2/' target='_blank' rel='noreferrer'><i class="fa-brands fa-linkedin icon"></i></a>
+            <a className='m-3' href='https://www.linkedin.com/in/patrick-nsolo-83b095a2/' target='_blank' rel='noreferrer'><i class="fa-brands fa-x-twitter icon"></i></a>
         </div>
       </div>
       <form action='/' method='POST'>

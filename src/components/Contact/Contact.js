d568dd4447356f90fa0fb586855dc4ac0ddc5c71
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-12-17 18:25:17 +0100

    update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
     <div className='contact-container'>
       <div className='contact' id='contact'>
         <h2>Lets Connect!</h2>
-        <p>If you would like to reach me, Please send me a message here and follow me on social media</p>  
+        <p>Follow us on all social media! Send us a Direct Message right here</p>  
         <div class="contact-links">
             <a className='m-3' href='https://github.com/patrick-nsolo' target='_blank' rel='noreferrer'><i class="fa-brands fa-github icon"></i></a>
             <a className='m-3' href='https://www.facebook.com/hotboi2thamost' target='_blank' rel='noreferrer'><i class="fa-brands fa-facebook icon"></i></a>
